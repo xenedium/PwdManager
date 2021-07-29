4348135ee718e48fd6300e2c9a57ef19f91b96bb
xenedium <aabderraziq@gmail.com>
2021-07-29 02:06:36 +0100

    updated for debuging

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
 #include <base64.hpp>
 
-
+using namespace std;
 
 int main(int argc, char** argv)
 {
-    printf("%s\n", B64_Encode("hello, world !"));
+    ifstream cin("input.txt");
+    ofstream cout("ouput.txt");
+
+    for (uint8_t i = 0; i < 0xff ; i++)
+    {
+        string inpt;
+        cin >> inpt;
+        cout << B64_Encode(inpt.c_str());
+    }
+    
+
+    return 0;
 }
