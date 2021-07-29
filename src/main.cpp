0afd0888a70d33304d8377a5f5a81ab743fb01dc
xenedium <aabderraziq@gmail.com>
2021-07-29 02:39:09 +0100

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,26 @@ using namespace std;
 int main(int argc, char** argv)
 {
     ifstream cin("input.txt");
-    ofstream cout("ouput.txt");
+    ofstream cout("inc_output.txt");
 
-    for (uint8_t i = 0; i < 0xff ; i++)
+    for (size_t i = 0; i < 10000 ; i++)
     {
         string inpt;
         cin >> inpt;
         cout << B64_Encode(inpt.c_str()) << endl;
     }
     
+    ifstream fcin("target.txt");
+    ofstream fcout("dec_output.txt");
+
+    for (size_t i = 0; i < 10000; i++)
+    {
+        string inpt;
+        fcin >> inpt;
+        fcout << B64_Decode(inpt.c_str()) << endl;
+    }
+
+    system("sha256sum target.txt inc_output.txt input.txt dec_output.txt");    
 
     return 0;
 }
